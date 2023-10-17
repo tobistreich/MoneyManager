cc90831a94d7b46dd7f24d5b41b7b85c18c458b9
tobistreich <tobias.streich@sidion.de>
2023-10-17 08:53:01 +0200

    bin add

--- MoneyManager/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ MoneyManager/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -184,7 +184,7 @@ namespace MoneyManager {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MoneyManager;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MoneyManager;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
